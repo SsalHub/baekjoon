36c864b62ac5418bb7c9e46f5c4abfd0944a8886
sjw000324@gmail.com <sjw000324@gmail.com>
2024-04-03 11:17:15 +0900

    1463 : trying

--- _1463/main.cpp
+++ _1463/main.cpp
@@ -1,12 +1,36 @@
 #include <iostream>
 #include <cstring>
 
+int dp(int x);
+
 int main()
 {
     std::ios_base::sync_with_stdio(false);
-    int x;
+    int x, result;
 
     std::cin >> x;
+    result = dp(x);
+    std::cout << result;
 
     return 0;
+}
+
+int dp(int x)
+{
+    const int len = x + 1;
+    int table[len][len];
+    int i, j, cell;
+
+    memset(table, x, len);
+
+    for (i = 1; i < len; i++)
+    {
+        for (j = 1; j < len; j++)
+        {
+            if (table[i][j] % 3 != 0)
+                table[i][j] = table[i - 1][j] - 1;
+            else
+                table[i][j] = table[i - 1][j] / 3;
+        }
+    }
 }
