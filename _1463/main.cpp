f496270281d16306279fa1db498c1575e4b048c1
sjw000324@gmail.com <sjw000324@gmail.com>
2024-04-06 09:46:35 +0900

    1463 : first try but wrong answer

--- _1463/main.cpp
+++ _1463/main.cpp
@@ -21,19 +21,22 @@ int dp(int x)
     int table[len][len];
     int i, j, cell;
 
-    memset(table, x, len);
+    memset(table, x, len * len);
 
     for (i = 1; i < len; i++)
     {
         for (j = 1; j < len; j++)
         {
-            if (table[i-1][j] % 3 != 0 && table[i-1][j] / 3 < table[i][j])
-                table[i][j] = table[i-1][j] / 3;
-            if (table[i][j-1] % 2 != 0 && table[i][j-1] / 2 < table[i][j])
-                table[i][j] = table[i][j-1] / 2;
-            
-            if (table[i-1][j-1] < table[i][j])
-                table[i][j] = table[i-1][j-1] - 1;
+            if (table[i - 1][j] % 3 != 0 && table[i - 1][j] / 3 < table[i][j])
+                table[i][j] = table[i - 1][j] / 3;
+            if (table[i][j - 1] % 2 != 0 && table[i][j - 1] / 2 < table[i][j])
+                table[i][j] = table[i][j - 1] / 2;
+
+            if (table[i - 1][j - 1] < table[i][j])
+                table[i][j] = table[i - 1][j - 1] - 1;
+
+            if (table[i][j] == 1)
+                return i + j - 1;
         }
     }
 }
