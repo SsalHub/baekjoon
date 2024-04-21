411f579da2f7889771919aba531a6af91d15a3c0
sjw000324@gmail.com <sjw000324@gmail.com>
2024-04-22 05:43:04 +0900

    1463 : trying

--- _1463/main.cpp
+++ _1463/main.cpp
@@ -19,24 +19,30 @@ int dp(int x)
 {
     const int len = x + 1;
     int table[len][len];
-    int i, j, cell;
+    int i, j, minimum = len;
 
+    if (x == 1)
+        return 1;
     memset(table, x, len * len);
 
     for (i = 1; i < len; i++)
     {
+        table[i][0] = table[i - 1][0]; // j = 0
         for (j = 1; j < len; j++)
         {
-            if (table[i - 1][j] % 3 != 0 && table[i - 1][j] / 3 < table[i][j])
-                table[i][j] = table[i - 1][j] / 3;
-            if (table[i][j - 1] % 2 != 0 && table[i][j - 1] / 2 < table[i][j])
-                table[i][j] = table[i][j - 1] / 2;
-
-            if (table[i - 1][j - 1] < table[i][j])
-                table[i][j] = table[i - 1][j - 1] - 1;
-
             if (table[i][j] == 1)
-                return i + j - 1;
+            {
+                minimum = j - 1 < minimum ? j - 1 : minimum;
+                continue;
+            }
+
+            if (table[i][j - 1] % 3 != 0)
+                table[i][j] = table[i - 1][j] / 3;
+            else if (table[i - 1][j] % 2 != 0)
+                table[i][j] = table[i][j - 1] / 2;
+            else if (table[i][j - 1] < table[i][j])
+                table[i][j] = table[i - 1][j - 1] - 1;
         }
     }
+    return minimum;
 }
