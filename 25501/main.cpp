30b1918487f07f52dcb1863e33b76b36db6eb837
sjw000324@gmail.com <sjw000324@gmail.com>
2024-05-13 23:19:19 +0900

    25501 : solved

--- 25501/main.cpp
+++ 25501/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <cstring>
-#include <string>
 
-#define _STRLEN_ 1000
+#define _STRLEN_ 1001
 
 int isPalindrome(const char *s);
 int recursion(const char *s, int l, int r, int recur_round);
@@ -10,25 +9,24 @@ int recursion(const char *s, int l, int r, int recur_round);
 int main()
 {
     std::ios_base::sync_with_stdio(false);
-    int n, is_pal, recur_round;
+    int n, result;
 
     std::cin >> n;
-    char pal[n][_STRLEN_ + 1];
+    std::cin.ignore();
+    char pal[n][_STRLEN_];
     for (int i = 0; i < n; i++)
     {
         std::cin.getline(pal[i], _STRLEN_);
-        recur_round = isPalindrome(pal[i]);
-        if (recur_round == 0)
-        {
-            is_pal = 1;
-            recur_round = strlen(pal[i]) / 2 + 1;
-        }
-        else
-        {
-            is_pal = 0;
-        }
+    }
 
-        std::cout << is_pal << " " << recur_round << "\n";
+    for (int i = 0; i < n; i++)
+    {
+        result = isPalindrome(pal[i]);
+
+        if (0 <= result)
+            std::cout << "0 " << result << "\n";
+        else
+            std::cout << "1 " << -result << "\n";
     }
 
     return 0;
@@ -39,11 +37,10 @@ int isPalindrome(const char *s)
     return recursion(s, 0, strlen(s) - 1, 1);
 }
 
-/** if succeess, return 0. or return recur_round. */
 int recursion(const char *s, int l, int r, int recur_round)
 {
     if (r <= l)
-        return 0;
+        return -recur_round;
     else if (s[l] != s[r])
         return recur_round;
     else
