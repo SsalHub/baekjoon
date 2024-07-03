0815ec54f088f0328979431e5df2c8a73286d837
SsalHub <sjw000324@gmail.com>
2024-07-03 20:45:03 +0900

    7579 : trying but runtime error

--- _7579/main.cpp
+++ _7579/main.cpp
@@ -1,53 +1,86 @@
 #include <iostream>
-#include <vector>
+#include <string.h>
+
+#define MEM 0
+#define COST 1
 
 using namespace std;
 
-int getMinCost(int n, int m, vector<int> mems, vector<int> costs);
+int partition(int arr[][2], int left, int right);
+void qsort(int arr[][2], int left, int right);
+void swap(int &num1, int &num2);
 
 int main()
 {
     std::ios_base::sync_with_stdio();
-    int n, m, mems_total = 0, costs_total = 0;
-    vector<int> mems, costs;
+    int apps[100][2], n, m, mem_sum = 0, cost_sum = 0;
 
     cin >> n >> m;
-    mems.resize(n);
-    costs.resize(n);
     for (int i = 0; i < n; i++)
     {
-        cin >> mems[i];
-        mems_total += mems[i];
+        cin >> apps[i][MEM];
     }
     for (int i = 0; i < n; i++)
     {
-        cin >> costs[i];
-        costs_total += costs[i];
+        cin >> apps[i][COST];
     }
 
-    getMinCost(n, mems_total - m, mems, costs);
+    qsort(apps, 0, n - 1);
+    for (int i = 0; i < n; i++)
+    {
+        if (m <= mem_sum)
+            break;
+        mem_sum += apps[i][MEM];
+        cost_sum += apps[i][COST];
+    }
+    cout << cost_sum;
+    return 0;
 }
 
-int getMinCost(int n, int m, vector<int> mems, vector<int> costs)
+int partition(int arr[][2], int left, int right)
 {
-    int table[n + 1][m + 1] = {
-        0,
-    };
-    int include, exclude;
+    int pivot = left, l = left, r = right + 1;
 
-    for (int i = 1; i < n + 1; i++)
+    do
     {
-        for (int j = 1; j < m + 1; j++)
+        do
         {
-            if (mems[i - 1] <= j)
-            {
-                include = table[i - 1][j] + costs[i - 1];
-                exclude = table[i][j - 1];
-            }
-            else
-            {
-                table[i][j] = table[i][j - 1];
-            }
+            l++;
+        } while (arr[l][COST] <= arr[pivot][COST]);
+
+        do
+        {
+            r--;
+        } while (arr[pivot][COST] <= arr[r][COST]);
+
+        if (l < r)
+        {
+            swap(arr[l][MEM], arr[r][MEM]);
+            swap(arr[l][COST], arr[r][COST]);
         }
+    } while (l < r);
+
+    if (arr[r][COST] < arr[pivot][COST])
+    {
+        swap(arr[pivot][MEM], arr[r][MEM]);
+        swap(arr[pivot][COST], arr[r][COST]);
     }
+    return r;
+}
+
+void qsort(int arr[][2], int left, int right)
+{
+    if (left < right)
+    {
+        int pivot = partition(arr, left, right);
+        qsort(arr, left, pivot - 1);
+        qsort(arr, pivot + 1, right);
+    }
+}
+
+void swap(int &num1, int &num2)
+{
+    int tmp = num1;
+    num1 = num2;
+    num2 = tmp;
 }
