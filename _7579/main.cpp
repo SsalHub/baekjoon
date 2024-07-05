118ee0a2c7b81014a319d2162379c17c8522fe98
SsalHub <sjw000324@gmail.com>
2024-07-05 21:35:03 +0900

    7579 : trying

--- _7579/main.cpp
+++ _7579/main.cpp
@@ -1,86 +1,52 @@
 #include <iostream>
 #include <string.h>
 
-#define MEM 0
-#define COST 1
-
-using namespace std;
-
-int partition(int arr[][2], int left, int right);
-void qsort(int arr[][2], int left, int right);
-void swap(int &num1, int &num2);
+int getMaxCost(int n, int m, int mems[], int costs[]);
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    int apps[101][2], n, m, mem_sum = 0, cost_sum = 0;
+    std::ios_base::sync_with_stdio(false);
+    int n, m, mems_total = 0, costs_total = 0;
 
-    cin >> n >> m;
+    std::cin >> n >> m;
+    int mems[n], costs[n];
     for (int i = 0; i < n; i++)
     {
-        cin >> apps[i][MEM];
+        std::cin >> mems[i];
+        mems_total += mems[i];
     }
     for (int i = 0; i < n; i++)
     {
-        cin >> apps[i][COST];
+        std::cin >> costs[i];
+        costs_total += costs[i];
     }
 
-    qsort(apps, 0, n - 1);
-    for (int i = 0; i < n; i++)
-    {
-        if (m <= mem_sum)
-            break;
-        mem_sum += apps[i][MEM];
-        cost_sum += apps[i][COST];
-    }
-    cout << cost_sum;
-    return 0;
+    std::cout << costs_total - getMaxCost(n, mems_total - m, mems, costs);
 }
 
-int partition(int arr[][2], int left, int right)
+int getMaxCost(int n, int m, int mems[], int costs[])
 {
-    int pivot = left, l = left, r = right + 1;
+    int table[n + 1][m + 1];
+    int include, exclude;
 
-    do
+    memset(table, 0, sizeof(table));
+
+    for (int i = 1; i < n + 1; i++)
     {
-        do
+        for (int j = 1; j < m + 1; j++)
         {
-            l++;
-        } while (arr[l][COST] <= arr[pivot][COST] && l < r);
-
-        do
-        {
-            r--;
-        } while (arr[pivot][COST] <= arr[r][COST] && l < r);
-
-        if (l < r)
-        {
-            swap(arr[l][MEM], arr[r][MEM]);
-            swap(arr[l][COST], arr[r][COST]);
+            if (mems[i - 1] <= j)
+            {
+                include = table[i - 1][j - mems[i - 1]] + costs[i - 1];
+                exclude = table[i - 1][j];
+                table[i][j] = std::max(include, exclude);
+            }
+            else
+            {
+                table[i][j] = table[i][j - 1];
+            }
         }
-    } while (l < r);
-
-    if (arr[r][COST] < arr[pivot][COST])
-    {
-        swap(arr[pivot][MEM], arr[r][MEM]);
-        swap(arr[pivot][COST], arr[r][COST]);
     }
-    return r;
-}
 
-void qsort(int arr[][2], int left, int right)
-{
-    if (left < right)
-    {
-        int pivot = partition(arr, left, right);
-        qsort(arr, left, pivot - 1);
-        qsort(arr, pivot + 1, right);
-    }
-}
-
-void swap(int &num1, int &num2)
-{
-    int tmp = num1;
-    num1 = num2;
-    num2 = tmp;
+    return table[n][m];
 }
