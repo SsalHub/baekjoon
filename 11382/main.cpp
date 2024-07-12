0ab13855068ff8ac4bd060e98a46a265e6ab92c3
SsalHub <sjw000324@gmail.com>
2024-07-12 22:22:27 +0900

    11382 : solved

---
+++ 11382/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(0);
+    unsigned long long A, B, C;
+    std::cin >> A >> B >> C;
+    std::cout << A + B + C << "\n";
+    return 0;
+}
