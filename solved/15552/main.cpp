3751f2305b451ce8b46a5daf839b3369bda5e1ea
SsalHub <sjw000324@gmail.com>
2024-07-13 00:02:11 +0900

    15552 : re-solved

--- solved/15552/main.cpp
+++ solved/15552/main.cpp
@@ -1,17 +1,15 @@
 #include <iostream>
 
-int main() {
-    std::cin.tie(NULL);
+int main()
+{
     std::ios_base::sync_with_stdio(false);
-
-    int n, a, b;
-
-    std::cin >> n;
-
-    for(int i=0; i<n; i++) {
-        std::cin >> a >> b;
-        std::cout << a + b << '\n';
+    std::cin.tie(0);
+    int T, A, B;
+    std::cin >> T;
+    while (T--)
+    {
+        std::cin >> A >> B;
+        std::cout << A + B << "\n";
     }
-
     return 0;
 }
