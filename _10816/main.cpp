f7dc6ef1defeea3e2c008f4295b23d58790058a1
SsalHub <sjw000324@gmail.com>
2024-07-12 16:48:33 +0900

    10816 : trying

--- _10816/main.cpp
+++ _10816/main.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 
+typedef struct Node
+{
+    int value;
+    int len;
+    struct Node *next;
+} Node;
+
 int main()
 {
-    std::ios_base::sync_with_stdio(false);
+    std::ios_base::sync_with_stdio(true);
     std::cin.tie(0);
     int N, M;
     std::cin >> N;
-    int arr[N];
     return 0;
 }
