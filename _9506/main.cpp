14263a8a452d52d8a9d6eda25fda579edcac3542
SsalHub <sjw000324@gmail.com>
2024-07-22 19:26:33 +0900

    9506 : trying

---
+++ _9506/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int n;
+    while (1)
+    {
+        cin >> n;
+        if (n == -1)
+            break;
+    }
+    return 0;
+}
