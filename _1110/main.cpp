90194d98be1e1288f8e2d61deaf40e15994938e9
SsalHub <sjw000324@gmail.com>
2025-02-19 19:28:36 +0900

    1110 : trying

--- _1110/main.cpp
+++ _1110/main.cpp
@@ -1,1 +1,22 @@
 #include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int input, n = -1, times = 0;
+
+    cin >> input;
+    while (input != n)
+    {
+        if (input < 10)
+            n = input + 0;
+        else
+            n = (input / 10) + (input % 10);
+        times++;
+    }
+    cout << times << '\n';
+    return 0;
+}
