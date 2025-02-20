f959bc205f56c3f674e1378ccaadede0c62d955e
SsalHub <sjw000324@gmail.com>
2025-02-21 03:49:30 +0900

    1145 : trying

--- _1145/main.cpp
+++ _1145/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#define SIZE 5
 
 using namespace std;
 
@@ -6,6 +7,11 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
+    int n[SIZE], i, j, k, mult;
 
+    for (i = 0; i < SIZE; i++)
+        cin >> n[i];
+    
+    cout << mult << '\n';
     return 0;
 }
