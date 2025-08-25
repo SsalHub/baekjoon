c51ecbfe638fc97422a608ab879f65c36b2b0756
SsalHub <sjw000324@gmail.com>
2025-08-25 21:18:47 +0900

    2293 : solved

---
+++ solved/2293/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int N, K;
+    int coin[101];
+    int dp[10001] = { 0 };
+    cin >> N >> K;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> coin[i];
+    }
+    dp[0] = 1;
+    for (int i = 0; i < N; i++)
+    {
+        for(int j = coin[i]; j <= K; j++)
+        {
+            dp[j] += dp[j - coin[i]];
+        }
+    }
+    cout << dp[K];
+    return 0;
+}
