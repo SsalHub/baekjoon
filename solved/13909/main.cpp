6a9687b111760f8df0d21b839953f7dd5dff0243
SsalHub <sjw000324@gmail.com>
2025-08-28 21:14:30 +0900

    13909 : solved

---
+++ solved/13909/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int N;
+    cin >> N;
+    cout << (int)sqrt(N);
+    return 0;
+}
+
